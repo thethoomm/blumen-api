cc74a903f399faa874ba02cd28d39fbd581a688a
thethoomm <thomas.santos02209@gmail.com>
2025-01-28 10:10:49 -0300

    feat: configure open cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { plantRoute } from "./routes/plants.route";
 import { plantTypeRoute } from "./routes/plant-type.route";
 import { env } from "../env";
@@ -6,6 +7,13 @@ import { env } from "../env";
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  })
+);
 
 app.use("/plants", plantRoute);
 app.use("/plant-types", plantTypeRoute);

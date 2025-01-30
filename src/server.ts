c2087516510cfcec49e70cf87ef0f5245e330445
thethoomm <thomas.santos02209@gmail.com>
2025-01-30 11:12:36 -0300

    fix: remove / from frontend url

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ const PORT = env.PORT || 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://thom-blumen.s3-website.us-east-2.amazonaws.com/",
+    origin: ["http://thom-blumen.s3-website.us-east-2.amazonaws.com"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
